e98f006ef329ae491f6259e57ba206382fc77e34
YISYISYISYIS <155742658+YISYISYISYIS@users.noreply.github.com>
2024-03-29 11:19:01 +0900

    [level 0] Title: 모음 제거, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    var answer = '';
+    removeStr = ["a","e","i","o","u"];
+    
+    for(let i = 0; i < my_string.length; i++){
+        if(!removeStr.includes(my_string[i])){
+            answer += my_string[i];
+        }
+    }
+    return answer;
+}
