96c06ee0377c3eed741bbf31c73e510c94eeb4da
YISYISYISYIS <155742658+YISYISYISYIS@users.noreply.github.com>
2024-03-31 11:31:36 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,6 +1,6 @@
 function solution(n) {
     var answer = 0;
     n = String(n).split('');
-    answer = n.reduce((num1,num2)=>(+(num1)) + (+(num2)),0);
+    answer = n.reduce((num1,num2) => Number(num1) + Number(num2),0);
     return answer;
 }
