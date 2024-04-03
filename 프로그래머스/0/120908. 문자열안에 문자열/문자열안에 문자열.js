9ff0534c07f94b44e9ec62702345b3b2eabe5d12
YISYISYISYIS <155742658+YISYISYISYIS@users.noreply.github.com>
2024-04-03 11:55:06 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.js
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -1,6 +1,17 @@
+// function solution(str1, str2) {
+//     var answer = 0;
+//     if(str1.includes(str2)){
+//         answer = 1;
+//     }else{
+//         answer = 2;
+//     }
+//     return answer;
+// }
+
+
 function solution(str1, str2) {
     var answer = 0;
-    if(str1.includes(str2)){
+    if(str1.split(str2).length > 1){
         answer = 1;
     }else{
         answer = 2;
