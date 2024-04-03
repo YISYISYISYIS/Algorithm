de949e8aa1a028ef7e7ad22b7202c0c9b9984b8c
YISYISYISYIS <155742658+YISYISYISYIS@users.noreply.github.com>
2024-04-03 11:03:37 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.js
@@ -0,0 +1,26 @@
+// function solution(n) {
+//     var answer = 0;
+//     for(let i = 0; i < n; i++){
+//         if(i*i === n){
+//             answer = 1;
+//             break;
+//         }else{
+//             answer = 2;
+//         }
+//     }
+//     return answer;
+// }
+
+
+function solution(n) {
+    var answer = 0;
+    for(let i = 0; i*i <= n; i++){
+        if(i*i === n){
+            answer = 1;
+        }
+    }
+    if(answer !== 1){
+        answer = 2;
+    }
+    return answer;
+}
