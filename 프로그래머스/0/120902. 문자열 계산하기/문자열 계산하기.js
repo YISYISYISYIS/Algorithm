45a53535656b516e885d605e5538fec80986092e
YISYISYISYIS <155742658+YISYISYISYIS@users.noreply.github.com>
2024-04-19 11:18:25 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,34 @@
+function solution(my_string) {
+    var answer = 0;
+    let tempNum = my_string.split(" ");
+    let saveValue = Number(tempNum[0]);
+    let indexSymbol = (tempNum[1]);
+    
+    for(let i = 1; i < tempNum.length; i+=2){
+        let currIndex = Number(tempNum[i+1]);
+        if(tempNum[i] === "+"){
+            saveValue += currIndex;
+        }else{
+            saveValue -= currIndex;
+        }
+    }
+    answer = saveValue;
+    return answer;
+}
+
+
+
+
+// function solution(my_string) {
+//     var answer = 0;
+//     let tempNum = my_string.split(" ");
+//     let indexZero = Number(tempNum[0]);
+//     let indexTwo = Number(tempNum[2]);
+    
+//     if(tempNum[1] === "+"){
+//         answer = indexZero + indexTwo;
+//     }else{
+//         answer = indexZero - indexTwo;
+//     }
+//     return answer;
+// }
