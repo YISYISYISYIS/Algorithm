dfb0c422894d935f23b9953672ac7b62fe59b7fc
YISYISYISYIS <155742658+YISYISYISYIS@users.noreply.github.com>
2024-05-10 00:16:04 +0900

    [level 1] Title: 음양 더하기, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -1,19 +1,15 @@
 function solution(absolutes, signs) {
-    var answer = 123456789;
-    //이게 true가 양수, false가 음수인가봄? 문제이해못함.
-    //둘다배열,길이 같고
-    //signs요소값 true,false를 조건문으로 확인후 absolutes에서 같은 인덱스를 
-    //-해주던지 나두던지하면될듯? 근데 -를 그냥붙이면 되나?
-    //머리가 복잡함 그냥 쓰면서 하기로
-    let tempNum = 0;
-    for(let i = 0; i < signs.length; i++){
+    var answer = 0;
+    //두 매개변수값들의 배열 길이는 같음
+    //반목분으로 돌면서 조건문으로 signs가 false/true일때
+    //absolutes에 +-를 붙여주고 그값을 answer에 누적시켜주면 될듯
+    for(let i = 0; i < absolutes.length; i++){
         if(signs[i] === false){
-            absolutes[i] = -absolutes[i];
-            tempNum += absolutes[i]
-        }else if(signs[i] === true){
-            tempNum += absolutes[i]
+            absolutes[i] = absolutes[i];
+            answer += absolutes[i] * -1;
+        }else{
+            answer += absolutes[i];
         }
     }
-    answer = tempNum;
     return answer;
 }
