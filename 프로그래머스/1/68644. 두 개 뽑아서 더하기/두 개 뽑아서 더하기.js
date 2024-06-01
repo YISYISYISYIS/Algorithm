cf417d3ad1cdcf21849908321d299e4978af95d0
YISYISYISYIS <155742658+YISYISYISYIS@users.noreply.github.com>
2024-06-02 00:25:40 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.52 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,5 +1,12 @@
 function solution(numbers) {
     var answer = [];
+    //배열안의 모든 수를 순회하면서 서로 더해야함 
+    //중첩반복문으로 j에 +i를 해줌으로써 j반복문의 조건을 
+    //배열의 끝까지 순회할수있게 만듬
+    //Set처음써봤는데 내장객채라고함. 중복값을 허용하지않음
+    //모든값을 일단 임시배열에 담고 Set활용해서 중복제거
+    //제거한값을 스프레드연산자로 다시 배열로 뿌려줌.
+    //오름차순정렬
     let tempArr = [];
     for(let i = 0; i < numbers.length; i++){
         for(let j = i+1; j < numbers.length; j++){
@@ -8,7 +15,7 @@ function solution(numbers) {
     }
     let arrSet = new Set(tempArr);
     let result = [...arrSet];
-    // console.log(result)
+    
     answer = result.sort((a,b)=> a-b)
     return answer;
 }
