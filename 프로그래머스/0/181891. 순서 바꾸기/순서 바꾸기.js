d641daaa579197f5969b1f4ec1e18f692dc7b996
YISYISYISYIS <155742658+YISYISYISYIS@users.noreply.github.com>
2024-07-26 00:35:06 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.js
@@ -0,0 +1,8 @@
+function solution(num_list, n) {
+    var answer = [];
+    let tempArr1 = num_list.slice(0,n)
+    let tempArr2 = num_list.splice(n)
+    answer.push(...tempArr2,...tempArr1)
+    
+    return answer;
+}
