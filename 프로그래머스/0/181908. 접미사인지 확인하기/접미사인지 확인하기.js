ec0e0cb8642be3b18a5200551e929a4b8e8dd8cd
YISYISYISYIS <155742658+YISYISYISYIS@users.noreply.github.com>
2024-08-01 00:14:54 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -0,0 +1,9 @@
+function solution(my_string, is_suffix) {
+    var answer = 0;
+    for(let i =0; i < my_string.length; i++){
+        if(my_string.slice(i) === is_suffix){
+            return answer = 1;
+        }
+    }
+    return answer;
+}
