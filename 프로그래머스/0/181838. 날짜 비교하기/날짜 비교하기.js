b30e8a2ca8d41488e4c293816eeb3c4fe12ca986
YISYISYISYIS <155742658+YISYISYISYIS@users.noreply.github.com>
2024-09-01 00:23:33 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.js
@@ -0,0 +1,23 @@
+function solution(date1, date2) {
+    var answer = 0;
+
+    if(date1[0] < date2[0]){
+        return answer = 1; 
+    }else if(date1[0] > date2[0]){
+        return answer = 0;
+    }
+
+    if(date1[1] < date2[1]){
+        return answer = 1; 
+    }else if(date1[1] > date2[1]){
+        return answer = 0;
+    }
+
+    if(date1[2] < date2[2]){
+        return answer = 1; 
+    }else if(date1[2] > date2[2]){
+        return answer = 0;
+    }
+
+    return 0; 
+}
